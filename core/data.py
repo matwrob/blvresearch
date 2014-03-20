7048dc665a027204af9a01b70efe6e8abee86b93
Mateusz Wroblewski <matwrob@gmail.com>
2014-03-20 08:44:48 +0100

    added data.py

--- core/data.py
+++ core/data.py
@@ -1,3 +1,5 @@
+import pandas as pd
+
 from fsprices.topcountry_model import get_many
 from fsprices.universe import Universe
 from memnews.core import NewsList
@@ -11,7 +13,7 @@ start_date = str(year_start) + "-01-01"
 end_date = str(year_end) + "-12-31"
 
 countries_eu = ['CH', 'DE', 'GB']
-securities_eu  = [s for s in runi if s.country in countries_eu]
+securities_eu = [s for s in runi if s.country in countries_eu]
 securities_us = [s for s in runi if s.country == 'US']
 entities_eu = [s.factset_entity_id for s in securities_eu]
 entities_us = [s.factset_entity_id for s in securities_us]
@@ -20,3 +22,9 @@ prices_eu = get_many(securities_eu, start_date, end_date)
 prices_us = get_many(securities_us, start_date, end_date)
 news_eu = NewsList.get_by_entity(entities_eu, start_date, end_date)
 news_us = NewsList.get_by_entity(entities_us, start_date, end_date)
+
+
+DATA_EU = dict()
+for e in entities_eu:
+    tmp = pd.join(prices_eu[e], news_eu[e])
+    DATA_EU[e]
