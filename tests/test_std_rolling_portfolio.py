d35fe432bfb2696565348f8aa4480f0b2a38e809
matwrob@gmail.com <wrobel84>
2014-04-11 16:39:32 +0200

    finished std_rolling_portfolio, added tests, some tests need to be added

--- tests/test_std_rolling_portfolio.py
+++ tests/test_std_rolling_portfolio.py
@@ -2,24 +2,19 @@ import pandas as pd
 import unittest
 
 from blvresearch.core.std_rolling_portfolio import (
-    get_dataframe_of_returns, get_resampled_returns,
-    get_testing_returns, get_winners, get_losers
+    get_dataframe_of_returns, get_sorted_returns,
+    get_winners, get_losers, get_portfolio_positions,
+    get_mean_returns
 )
 
 from concat.core.utils import load_object
 
 
-PRICE_DATA, META_DATA = load_object('blvresearch/tests/mock_data.pickle')
-PRICE_DATA['abs_ret'] = PRICE_DATA['rel_ret'] + PRICE_DATA['bench']
-PRICE_DATA = {'000C7F-E': PRICE_DATA, '001C7F-E': PRICE_DATA,
-              '002C7F-E': PRICE_DATA, '003C7F-E': PRICE_DATA,
-              '004C7F-E': PRICE_DATA, '005C7F-E': PRICE_DATA,
-              '006C7F-E': PRICE_DATA, '007C7F-E': PRICE_DATA,
-              '008C7F-E': PRICE_DATA, '009C7F-E': PRICE_DATA,
-              '010C7F-E': PRICE_DATA, '011C7F-E': PRICE_DATA,
-              '012C7F-E': PRICE_DATA, '013C7F-E': PRICE_DATA,
-              '014C7F-E': PRICE_DATA, '015C7F-E': PRICE_DATA}
+PRICE_DATA = load_object('/vagrant/blvresearch/tests/mock_jegadeesh.pickle')
 
+def ts(year, month, day):
+    dt = pd.datetime(year, month, day, 0, 0, 0)
+    return pd.Timestamp(dt, tz='UTC')
 
 class TestGetDataFrameOfReturns(unittest.TestCase):
 
@@ -27,79 +22,149 @@ class TestGetDataFrameOfReturns(unittest.TestCase):
         result = get_dataframe_of_returns(PRICE_DATA)
         self.assertIsInstance(result, pd.DataFrame)
         self.assertEqual(set(result.columns), set(PRICE_DATA.keys()))
-        self.assertEqual(result.shape, (754, 16))
+        self.assertEqual(result.shape, (754, 10))
+        self.assertEqual(result['000C7F-E']['2013-12-31'][0],
+                         0.014190946109989659)
 
     def test_get_alpha(self):
         result = get_dataframe_of_returns(PRICE_DATA, 'alpha')
         self.assertIsInstance(result, pd.DataFrame)
         self.assertEqual(set(result.columns), set(PRICE_DATA.keys()))
-        self.assertEqual(result.shape, (754, 16))
+        self.assertEqual(result.shape, (754, 10))
+        self.assertEqual(result['000C7F-E']['2013-12-31'][0],
+                         0.011690812716191207)
 
 
-class TestGetResampledReturns(unittest.TestCase):
-
-    def test_get_monthly(self):
-        result = get_resampled_returns(PRICE_DATA)
-        self.assertIsInstance(result, pd.DataFrame)
-        self.assertEqual(set(result.columns), set(PRICE_DATA.keys()))
-        self.assertEqual(result.shape, (36, 16))
-        self.assertEqual(result['000C7F-E'][0], 0.050654449496584694)
-
-    def test_get_quarterly(self):
-        result = get_resampled_returns(PRICE_DATA, freq='Q')
-        self.assertIsInstance(result, pd.DataFrame)
-        self.assertEqual(set(result.columns), set(PRICE_DATA.keys()))
-        self.assertEqual(result.shape, (12, 16))
-        self.assertEqual(result['000C7F-E'][0], 0.077370585329502986)
-
-
-class TestGetTestingReturns(unittest.TestCase):
+class TestGetSortedReturns(unittest.TestCase):
 
     def test_get_1_month(self):
-        df = get_resampled_returns(PRICE_DATA)
-        result = get_testing_returns(df)
-        self.assertEqual(result.shape, (36, 16))
-        self.assertEqual(str(result.index[0])[:10], '2011-01-31')
-        self.assertEqual(result.loc['2011-01-31'][0], 0.050654449496584694)
+        df = get_dataframe_of_returns(PRICE_DATA)
+        resampled = df.resample('M', how=sum)
+        result = get_sorted_returns(resampled, test_per=1)
+
+        for k, v in result.items():
+            self.assertIsInstance(k, pd.tslib.Timestamp)
+            self.assertIsInstance(v, pd.Series)
+        self.assertEqual(len(result), 36)
+        self.assertEqual(len(result[ts(2011, 1, 31)]), 10)
+        self.assertEqual(result[ts(2011, 1, 31)]['000C7F-E'],
+                         0.028940836516168474)
 
     def test_get_2_months(self):
-        df = get_resampled_returns(PRICE_DATA)
-        result = get_testing_returns(df, test_periods=2)
-        self.assertEqual(result.shape, (35, 16))
-        self.assertEqual(str(result.index[0])[:10], '2011-02-28')
-        self.assertEqual(result.loc['2011-02-28'][0], 0.09077361545684362)
+        df = get_dataframe_of_returns(PRICE_DATA)
+        resampled = df.resample('M', how=sum)
+        result = get_sorted_returns(resampled, test_per=2)
+
+        for k, v in result.items():
+            self.assertIsInstance(k, pd.tslib.Timestamp)
+            self.assertIsInstance(v, pd.Series)
+        self.assertEqual(len(result), 35)
+        self.assertEqual(len(result[ts(2011, 2, 28)]), 10)
+        self.assertEqual(result[ts(2011, 2, 28)]['000C7F-E'],
+                         0.061647021123041996)
 
     def test_get_3_months(self):
-        df = get_resampled_returns(PRICE_DATA)
-        result = get_testing_returns(df, test_periods=3)
-        self.assertEqual(result.shape, (34, 16))
-        self.assertEqual(str(result.index[0])[:10], '2011-03-31')
-        self.assertEqual(result.loc['2011-03-31'][0], 0.077370585329503014)
+        df = get_dataframe_of_returns(PRICE_DATA)
+        resampled = df.resample('M', how=sum)
+        result = get_sorted_returns(resampled, test_per=3)
+
+        for k, v in result.items():
+            self.assertIsInstance(k, pd.tslib.Timestamp)
+            self.assertIsInstance(v, pd.Series)
+        self.assertEqual(len(result), 34)
+        self.assertEqual(len(result[ts(2011, 3, 31)]), 10)
+        self.assertEqual(result[ts(2011, 3, 31)]['000C7F-E'],
+                         0.021173995271057768)
 
 
-class TestGetWinners(unittest.TestCase):
+class TestWinnersLosers(unittest.TestCase):
 
-    def test_get_winners(self):
-        abs_ret = get_resampled_returns(PRICE_DATA)
-        test_ret = get_testing_returns(abs_ret)
-        result = get_winners(test_ret)
+    def test_1M_terciles(self):
+        df = get_dataframe_of_returns(PRICE_DATA)
+        resampled = df.resample('M', how=sum)
 
-        for date, portf in result.items():
-            self.assertEqual(len(portf), 5)
+        winners = get_winners(resampled, test_per=1, no_of_quantiles=3)
+        self.assertEqual(winners[ts(2011, 1, 31)],
+                         ['000Y8N-E', '000KYG-E', '002Q0G-E'])
+
+        losers = get_losers(resampled, test_per=1, no_of_quantiles=3)
+        self.assertEqual(losers[ts(2011, 1, 31)],
+                         ['000KN2-E', '069J8N-E', '000CS1-E'])
+
+    def test_2M_quintiles(self):
+        df = get_dataframe_of_returns(PRICE_DATA)
+        resampled = df.resample('M', how=sum)
+
+        winners = get_winners(resampled, test_per=2, no_of_quantiles=5)
+        self.assertEqual(winners[ts(2011, 2, 28)],
+                         ['002Q0G-E', '000Y8N-E'])
+
+        losers = get_losers(resampled, test_per=2, no_of_quantiles=5)
+        self.assertEqual(losers[ts(2011, 2, 28)],
+                         ['000KN2-E', '000YMS-E'])
 
 
-class TestGetLosers(unittest.TestCase):
+class TestGetPorfolioPositions(unittest.TestCase):
 
-    def test_get_losers(self):
-        abs_ret = get_resampled_returns(PRICE_DATA)
-        test_ret = get_testing_returns(abs_ret, test_periods=2)
-        result = get_losers(test_ret, test_periods=2, no_of_quantiles=8)
+    def test_1M_testing_1M_holding_terciles(self):
+        df = get_dataframe_of_returns(PRICE_DATA)
+        resampled = df.resample('M', how=sum)
 
-        for date, portf in result.items():
-            self.assertEqual(len(portf), 2)
+        winners = get_winners(resampled, test_per=1, no_of_quantiles=3)
+        result = get_portfolio_positions(resampled, winners, hold_per=1)
 
+        self.assertIsInstance(result, pd.DataFrame)
+
+        self.assertEqual(result.index[0], ts(2011, 1, 31))
+
+        first_row = result.loc[ts(2011, 1, 31)]
+        self.assertEqual(len(first_row[first_row == True]), 0)
+
+        second_row = result.loc[ts(2011, 2, 28)]
+        self.assertEqual(set(second_row[second_row == True].index),
+                         set(['000Y8N-E', '000KYG-E', '002Q0G-E']))
+
+    def test_2M_testing_2M_holding_quintiles(self):
+        df = get_dataframe_of_returns(PRICE_DATA)
+        resampled = df.resample('M', how=sum)
+
+        losers = get_losers(resampled, test_per=2, no_of_quantiles=5)
+        result = get_portfolio_positions(resampled, losers, hold_per=2)
+
+        self.assertIsInstance(result, pd.DataFrame)
+
+        self.assertEqual(result.index[0], ts(2011, 2, 28))
+
+        first_row = result.loc[ts(2011, 2, 28)]
+        self.assertEqual(len(first_row[first_row == True]), 0)
+
+        second_row = result.loc[ts(2011, 3, 31)]
+        self.assertEqual(set(second_row[second_row == True].index),
+                         set(['000KN2-E', '000YMS-E']))
+
+
+class TestGetMeanReturns(unittest.TestCase):
+
+    def test_1M_testing_1M_holding_terciles(self):
+        df = get_dataframe_of_returns(PRICE_DATA)
+        resampled = df.resample('M', how=sum)
+        winners = get_winners(resampled, test_per=1, no_of_quantiles=3)
+        positions = get_portfolio_positions(resampled, winners, hold_per=1)
+        result = get_mean_returns(resampled, positions, hold_per=1)
+        self.assertTrue(False)
+
+
+
+    def test_2M_testing_2M_holding_quintiles(self):
+        self.assertTrue(False)
 
 class TestGetJegadeeshReturns(unittest.TestCase):
 
-    def test_get(self):
-        pass
+    def test_get_winners_returns(self):
+        self.assertTrue(False)
+
+    def test_get_losers_returns(self):
+        self.assertTrue(False)
+
+    def test_get_wml_returns(self):
+        self.assertTrue(False)
