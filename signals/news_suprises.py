e11a898fb542a753864e0082553be13e48bf87ce
matwrob@gmail.com <wrobel84>
2014-06-14 17:15:38 +0200

    news signals

--- signals/news_suprises.py
+++ signals/news_suprises.py
@@ -9,14 +9,12 @@ TODAY = dt.datetime.today()
 
 def get_suprises(data, entity_id):
     result = pd.Series(index=data.index)
-    enough_days_before_event = lambda x: x - 22 > 0
     days_to_check = _get_days_to_check(data, entity_id)
     for i, day in enumerate(days_to_check.index):
-        if enough_days_before_event(i):
-            if _is_positive_suprise(data, day):
-                result[day] = True
-            elif _is_negative_suprise(data, day):
-                result[day] = False
+        if _is_positive_suprise(data, day):
+            result[day] = True
+        elif _is_negative_suprise(data, day):
+            result[day] = False
     return _remove_consecutive_values(result)
 
 
@@ -30,19 +28,16 @@ def _remove_consecutive_values(series):
 def _get_days_to_check(df, entity_id):
     result = pd.Series(index=df.index)
     ms = MeanSigma(df['alpha'])
-    news = df['news'].map(lambda x: True if isinstance(x, NewsList) else False)
-    for date, row in df[news].iterrows():
+    for date, row in df.iterrows():
         if (_has_relevant_news(row['news'], entity_id) and
-            abs(row['alpha']) > ms.mean + 2 * ms.sigma):
+            abs(row['alpha']) > ms.mean):
             result[date] = True
-    return result
+    return result.dropna()
 
 
 def _has_relevant_news(news_list, entity_id):
-    for n in news_list:
-        if n['relevance'][entity_id]['score'] > 0.3:
-            return True
-    return False
+    if isinstance(news_list, list):
+        return [n for n in news_list if n['irrelevant'] == False]
 
 
 def _is_negative_suprise(df, date):
